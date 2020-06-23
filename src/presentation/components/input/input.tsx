6bdd956f087cbd99fff9a1a8c469c06033306d63
danilomacb <danilo.bakun@gmail.com>
2020-06-23 08:29:41 -0300

    feat: ensure login present valid state if password is valid

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -17,10 +17,10 @@ const Input: React.FC<Props> = (props: Props) => {
     })
   }
   const getStatus = (): string => {
-    return '🔴️'
+    return error ? '🔴️' : '🔵️'
   }
   const getTitle = (): string => {
-    return error
+    return error || 'Tudo certo!'
   }
 
   return (
